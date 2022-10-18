f2cbdaabf625d9382524da98eabc51c6d7c8b065
fahaddd-git <awan.fahad@gmail.com>
2022-10-18 12:20:34 -0400

    added pytest xfail decorator for placeholder tests

--- services/utilities/tests/sample_test.py
+++ services/utilities/tests/sample_test.py
@@ -1,4 +1,8 @@
+import pytest
+
+
+@pytest.mark.xfail(reason="initial pipeline setup")
 def test_sample():
     """Exists for initial ci pipeline setup.
     Add your tests somewhere in the tests dir and delete this"""
-    assert 5 == 5
+    assert False
