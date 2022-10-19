72c72aa3cb48143e07c919fe69f0fa731cff048d
Donato Quartuccia <66536458+donatosaur@users.noreply.github.com>
2022-10-19 19:42:35 -0400

    Create frontend layout

---
+++ frontend/src/utils/audio.ts
@@ -0,0 +1,7 @@
+/**
+ * Validates and preprocesses an uploaded audio file
+ * @return Promise that resolves to a 30-second clip of the passed audio file
+ */
+export async function processAudioFile (audioFile: File): Promise<File> {
+  throw new Error('Not implemented');
+}
