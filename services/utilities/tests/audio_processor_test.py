b9914daa081eca8bd0da07d74101e940d8d17e00
fahaddd-git <awan.fahad@gmail.com>
2022-10-21 16:36:15 -0400

    added type-hints, driver function, test fix, lock merge

--- services/utilities/tests/audio_processor_test.py
+++ services/utilities/tests/audio_processor_test.py
@@ -3,6 +3,7 @@ import os
 import librosa
 import numpy as np
 import pytest
+from PIL import Image
 from utilities.audio_processor import app
 
 TEST_FILE_PATH = "tests/test_files/classical.00000.wav"
@@ -29,17 +30,15 @@ def test_stream_spectrogram(load_librosa_mel_spectrogram, load_stream_spectrogra
 
 def test_transform_spectrogram(load_stream_spectrogram):
     """Tests if min and max are [0-255]"""
-    assert np.any((load_stream_spectrogram <= 255) | (load_stream_spectrogram >= 0))
+    assert np.any((load_stream_spectrogram >= 0) & (load_stream_spectrogram <= 255))
 
 
-def test_save_spectrogram_to_image(load_stream_spectrogram):
+def test_convert_sound_to_image():
     """Tests saving log spectrogram as image in default param path"""
-    file_name = "test_file"
-    log_spectrogram = app.log_spectrogram(load_stream_spectrogram)
+    result = app.convert_sound_to_image(TEST_FILE_PATH)
     default_param_file_path = app.save_spectrogram_to_image.__defaults__[0]
-    app.save_spectrogram_to_image(log_spectrogram, file_name)
-    file = f"{default_param_file_path}/{file_name}.png"
+    file = f"{default_param_file_path}/classical.00000.png"
     if not os.path.isfile(file):
         assert False
     os.remove(file)
-    assert True
+    assert isinstance(result, Image.Image)
--- services/utilities/utilities/audio_processor/app.py
+++ services/utilities/utilities/audio_processor/app.py
@@ -1,32 +1,19 @@
-from os import makedirs
+from pathlib import Path
+from typing import Optional
 
 import librosa
 import librosa.display
-
-# import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
 
-filename = "tests/test_files/classical.00000.wav"
 
-
-def stream_spectrogram(file_path, n_fft=2048, hop_length=2048, frame_length=2048):
+def stream_spectrogram(
+    file_path: str, n_fft: int = 2048, hop_length: int = 2048, frame_length: int = 2048
+) -> np.ndarray:
     """
     Converts an audio stream to a mel spectrogram by way of STFTs.
     An explanation of the reasoning behind this workflow can be found at
     https://dsp.stackexchange.com/questions/76637/do-mel-spectrograms-of-two-audios-have-linear-property
-
-    :param file_path: Path to audio file
-    :type file_path: str
-    :param n_fft: Length of the FFT window
-    :type n_fft: int, optional
-    :param hop_length: Number of samples between successive frames
-    :type hop_length: int, optional
-    :param frame_length: The number of samples per frame
-    :type frame_length: int, optional
-
-    :return: Power Mel Spectrogram constructed from audio file with n_mels=128
-    :rtype: numpy.ndarray
     """
 
     stream = librosa.stream(
@@ -50,64 +37,60 @@ def stream_spectrogram(file_path, n_fft=2048, hop_length=2048, frame_length=2048
         S=abs_value, sr=sr, hop_length=2048, center=False, n_mels=128
     )
 
-    # displays mel spectrogram
-    # librosa.display.specshow(mel_spectro, sr=sr, x_axis="time", y_axis="mel", hop_length=2048)
-    # plt.colorbar()
-    # plt.show()
-
     return mel_spectro
 
 
-def transform_spectrogram(spectrogram):
+def transform_spectrogram(spectrogram: np.ndarray) -> np.ndarray:
     """
     Applies standardization and contrast normalization to spectrogram.
     This is so that the spectrogram, a numpy array, can be cast as a greyscale image.
-
-    :param spectrogram: Spectrogram to be transformed
-    :type spectrogram: numpy.ndarray
-
-    :return: Standardized and Normalized spectrogram.  All values between [0-255]
-    :rtype: numpy.ndarray(type=uint8)
     """
     # standardize data https://en.wikipedia.org/wiki/Standard_score
     standardized = (spectrogram - spectrogram.mean()) / spectrogram.std()
     standardized_min = standardized.min()
     # contrast normalization for greyscale image
-    # https://stackoverflow.com/questions/70783357/how-do-i-normalize-the-pixel-value-of-an-image-to-01https://stackoverflow.com/questions/70783357/how-do-i-normalize-the-pixel-value-of-an-image-to-01
+    # https://stackoverflow.com/questions/70783357/how-do-i-normalize-the-pixel-value-of-an-image-to-01
     spec_scaled = (standardized - standardized_min) / (standardized.max() - standardized_min) * 255
     return spec_scaled.astype(np.uint8)
 
 
-def save_spectrogram_to_image(spectrogram, file_name, file_path="utilities/audio_processor/images"):
-    """Saves spectrogram as .png file in greyscale
-
-    :param spectrogram: Spectrogram to be saved as .png
-    :type spectrogram: numpy.ndarray
-    :param file_name: Name of file to be saved without extension
-    :type file_name: str
-    :param file_path: Where to save the image file:
-    :type file_path: str, optional
-
-    :return: None, Image saved to file_path with file_name as .png
-    :rtype: NoneType
-    """
+def save_spectrogram_to_image(
+    spectrogram: np.ndarray,
+    file_name: str,
+    output_file_path: str = "utilities/audio_processor/images",
+) -> Image:
+    """Saves spectrogram as .png file in greyscale"""
 
     rescaled = transform_spectrogram(spectrogram)
-    makedirs(file_path, exist_ok=True)
+    Path(output_file_path).mkdir(exist_ok=True)
     image = Image.fromarray(rescaled)
-    image.save(f"{file_path}/{file_name}.png")
+    image.save(f"{output_file_path}/{file_name}.png")
+    return image
 
 
-def log_spectrogram(mel_spectrogram):
+def log_spectrogram(mel_spectrogram: np.ndarray) -> np.ndarray:
     """Returns log mel spectrogram.
     In a log mel spectrogram, amplitude is converted to decibels which follows a logarithmic scale.
-
-    :param mel_spectrogram: Mel spectrogram to be converted
-    :type mel_spectrogram: numpy.ndarray
-
-    :return: Log Mel Spectrogram
-    :rtype: numpy.ndarray
     """
     log_mel_spectrogram = librosa.power_to_db(mel_spectrogram, ref=np.max)
-
     return log_mel_spectrogram
+
+
+def convert_sound_to_image(
+    sound_file_path: str,
+    output_name_no_ext: Optional[None] = None,
+    librosa_options: Optional[dict] = None,
+) -> Image:
+    """
+     Interface for converting sound to image.
+    Default librosa_options are: n_fft: int = 2048, hop_length: int = 2048, frame_length: int = 2048
+    """
+    spectrogram = (
+        stream_spectrogram(sound_file_path, **librosa_options)
+        if librosa_options
+        else stream_spectrogram(sound_file_path)
+    )
+    log_spec = log_spectrogram(spectrogram)
+    file_name = output_name_no_ext or Path(sound_file_path).stem
+    image = save_spectrogram_to_image(log_spec, file_name)
+    return image
