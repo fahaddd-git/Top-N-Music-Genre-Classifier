516e2d142d8a50d419a46ccd49a72566f676a249
erietz <ewrietz@gmail.com>
2022-10-23 14:28:11 -0500

    fix logic

--- frontend/src/components/upload-button/UploadButton.tsx
+++ frontend/src/components/upload-button/UploadButton.tsx
@@ -1,39 +1,15 @@
+import { processAudioFile } from '../../utils/';
 import { Button } from '@mui/material';
 import FileUploadIcon from '@mui/icons-material/FileUpload';
 
 export const UploadButton = () => {
-  const minimumAudioLength = 30;  // seconds
-
   const onChange = (event: React.FormEvent) => {
     const files = (event.target as HTMLInputElement).files;
     if (files != null && files.length > 0) {
-      const theFile = files[0];
-      console.log('file is ', theFile);
-      decode(theFile);
-    }
-  };
-
-  const decode = (f: File) => {
-    const context = new AudioContext();
-    const reader = new FileReader();
-
-    reader.readAsArrayBuffer(f);
-    reader.onload = () => {
-      const buffer = reader.result as ArrayBuffer;
-      context.decodeAudioData(buffer)
-        .then(audioData => {
-          if (audioData.duration < minimumAudioLength) {
-            alert(`Audio file must be at least ${minimumAudioLength}s`);
-          } else {
-            getPrediction(audioData);
-          }
-        })
+      processAudioFile(files[0])
+        .then(file => console.log(file))
         .catch(err => console.error(err));
-    };
-  };
-
-  const getPrediction = (f: AudioBuffer) => {
-    console.log('TODO: call /api/predict-genres with', f);
+    }
   };
 
   return (
