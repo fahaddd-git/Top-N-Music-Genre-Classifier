12b9c8aab19733734891e3f0938c8c506b82c8fa
Donato Quartuccia <66536458+donatosaur@users.noreply.github.com>
2022-11-06 13:47:32 -0500

    Mock database for tests

--- services/neural-network/tests/conftest.py
+++ services/neural-network/tests/conftest.py
@@ -1,28 +1,41 @@
-from functools import cache
-from unittest.mock import patch
+import csv
+from datetime import datetime
+from pathlib import Path
 
 import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from utilities.db.models import _BaseModel
-
-
-@cache
-def patched_session():
-    engine = create_engine("sqlite://")
-    # see https://docs.sqlalchemy.org/en/14/core/metadata.html#sqlalchemy.schema.MetaData.create_all
-    _BaseModel.metadata.create_all(bind=engine)  # noinspection PyProtectedMember
-
-    # first row has column names, so skip those
-    engine.execute(f".import --csv --skip 1 {'./fixtures/genres.csv'} Genres")
-    engine.execute(f".import --csv --skip 1 {'./fixtures/spectrograms.csv'} Spectrograms")
-    session = sessionmaker(bind=engine)
-    return session
+from utilities.db.models import Genre, Spectrogram, _BaseModel
 
 
 @pytest.fixture(scope="session", autouse=True)
-def patch_test_database():
-    # this may not actually work, but it's essentially what has to happen - the question is how
-    # to patch the correct thing so that this gets patched *wherever* it's used
-    with patch("utilities.db.connector.sqlite_session", return_value=patched_session):
-        yield
+def patched_session():
+    """Patched sessionmaker to in-memory database"""
+    engine = create_engine("sqlite://")
+    # see https://docs.sqlalchemy.org/en/14/core/metadata.html#sqlalchemy.schema.MetaData.create_all
+    _BaseModel.metadata.create_all(bind=engine)
+    session = sessionmaker(bind=engine)
+
+    # effectively engine.execute(".import --csv --skip 1 ./fixtures/genres.csv Genres")
+    # except that the dot-directives aren't SQL statements and require using the sqlite3 binary
+    genres_csv_path = Path(__file__).resolve().parent / "fixtures/genres.csv"
+    with session.begin() as current_session, open(genres_csv_path) as genres:
+        genre_rows = csv.DictReader(genres)
+        objects = (Genre(**row) for row in genre_rows)
+        current_session.add_all(objects)
+
+    # engine.execute(".import --csv --skip 1 ./fixtures/spectrograms.csv Spectrograms")
+    spectrograms_csv_path = Path(__file__).resolve().parent / "fixtures/spectrograms.csv"
+    with session.begin() as current_session, open(spectrograms_csv_path) as spectrograms:
+        spectrogram_rows = csv.DictReader(spectrograms)
+        overwrite_args = {"image_data": b"", "last_modified": datetime.now()}
+        objects = (Spectrogram(**{**row, **overwrite_args}) for row in spectrogram_rows)
+        current_session.add_all(objects)
+
+    return session
+
+
+@pytest.fixture(scope="session")
+def num_spectrograms(patched_session):
+    with patched_session.begin() as session:
+        return session.query(Spectrogram).count()
--- services/neural-network/tests/test_data_ingestion_helpers.py
+++ services/neural-network/tests/test_data_ingestion_helpers.py
@@ -1,34 +1,55 @@
+from typing import Final
+from unittest.mock import patch
+
 import pytest
 from neural_network.data_ingestion_helpers import _get_genre_occurrences, train_test_split
-from utilities.db.connector import sqlite_session
-from utilities.db.models import Spectrogram
 
-with sqlite_session().begin() as session:
-    N = session.query(Spectrogram).count()
-
-PARAM_NAMES = "train_frac,num_train,num_test"
-PARAMS = [
-    (0.5, 0.5 * N, 0.5 * N),
-    (0.6, 0.6 * N, 0.4 * N),
-    (0.4, 0.4 * N, 0.6 * N),
-    (0.75, 0.75 * N, 0.25 * N),
-    (0.813, 0.82 * N, 0.18 * N),
-]
+MODULE_PATH: Final = "neural_network.data_ingestion_helpers"
 
 
-@pytest.mark.parametrize(PARAM_NAMES, PARAMS)
-def test_train_test_split(train_frac, num_train, num_test):
+@pytest.fixture(scope="function", autouse=True)
+def patch_session(patched_session):
+    with patch(f"{MODULE_PATH}.sqlite_session", return_value=patched_session):
+        yield
+
+
+@pytest.mark.parametrize(
+    "train_frac, num_train, num_test",
+    [
+        (0.500, 0.50, 0.50),
+        (0.600, 0.60, 0.40),
+        (0.400, 0.40, 0.60),
+        (0.750, 0.75, 0.25),
+        (0.813, 0.82, 0.18),
+    ],
+)
+@patch(f"{MODULE_PATH}.tf.io.decode_image")
+def test_train_test_split(patched_tf_io_decode, train_frac, num_train, num_test, num_spectrograms):
+    patched_tf_io_decode.return_value = None
     spectrogram_data = train_test_split(train_frac)
-    assert len(spectrogram_data.train_data) == num_train
-    assert len(spectrogram_data.train_labels) == num_train
-    assert len(spectrogram_data.test_data) == num_test
-    assert len(spectrogram_data.test_labels) == num_test
+    assert len(spectrogram_data.train_data) == num_train * num_spectrograms
+    assert len(spectrogram_data.train_labels) == num_train * num_spectrograms
+    assert len(spectrogram_data.test_data) == num_test * num_spectrograms
+    assert len(spectrogram_data.test_labels) == num_test * num_spectrograms
 
 
-@pytest.mark.parametrize(PARAM_NAMES, PARAMS)
-def test_labels_are_correct(train_frac, num_train, num_test):
-    spectrogram_data = train_test_split(train_fraction=train_frac)
+@pytest.mark.parametrize(
+    "train_frac, num_train, num_test",
+    [
+        (0.500, 0.50, 0.50),
+        (0.600, 0.60, 0.40),
+        (0.400, 0.40, 0.60),
+        (0.750, 0.75, 0.25),
+        (0.813, 0.82, 0.18),
+    ],
+)
+@patch(f"{MODULE_PATH}.tf.io.decode_image")
+def test_labels_are_correct(
+    patched_tf_io_decode, train_frac, num_train, num_test, num_spectrograms
+):
+    patched_tf_io_decode.return_value = None
+    spectrogram_data = train_test_split(train_frac)
     for id, count in _get_genre_occurrences().items():
         label = id - 1
-        assert spectrogram_data.train_labels.count(label) == num_train * count / N
-        assert spectrogram_data.test_labels.count(label) == num_test * count / N
+        assert spectrogram_data.train_labels.count(label) == num_train * count
+        assert spectrogram_data.test_labels.count(label) == num_test * count
