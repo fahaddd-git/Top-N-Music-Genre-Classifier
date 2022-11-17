3bd296839b916bcbe280153daaf3e6b9bb596c59
Donato Quartuccia <66536458+donatosaur@users.noreply.github.com>
2022-11-17 09:32:59 -0500

    Support audio files exceeding 30 seconds (#52)
    Load genre labels from JSON
    Average predictions of 30-second slices of the audio file (limited to first 3 segments)
    Use NamedTemporaryFile for temporary file handling

--- services/prediction-api/prediction_api/routers/predict_genre.py
+++ services/prediction-api/prediction_api/routers/predict_genre.py
@@ -1,15 +1,15 @@
 import io
-import os
-import random
-import string
+import json
 from functools import cache
 from pathlib import Path
+from tempfile import NamedTemporaryFile
 from typing import Final
 
+import numpy as np
 import tensorflow as tf
 from fastapi import APIRouter, HTTPException, UploadFile, status
 from pydantic import BaseModel
-from utilities.audio_processor import convert_sound_to_image
+from utilities.audio_processor import generate_sound_images
 
 # IANA is the official registry of MIME media types.
 # See https://www.iana.org/assignments/media-types/media-types.xhtml and
@@ -18,9 +18,10 @@ from utilities.audio_processor import convert_sound_to_image
 SUPPORTED_AUDIO_CONTENT_SUBTYPES: Final = frozenset(
     (
         "mpeg",
-        "oog",
         "wav",
         "x-wav",
+        "flac",
+        "x-flac",
     )
 )
 
@@ -61,43 +62,63 @@ def is_supported_audio_file(file: UploadFile) -> bool:
     return mime_type == "audio" and mime_subtype in SUPPORTED_AUDIO_CONTENT_SUBTYPES
 
 
-def predict(filestream: bytes) -> dict:
-    model = load_model()
-    labels_to_str = {
-        0: "disco",
-        1: "country",
-        2: "rock",
-        3: "pop",
-        4: "jazz",
-        5: "metal",
-        6: "blues",
-        7: "reggae",
-        8: "classical",
-        9: "hiphop",
-    }
-    random_string = "".join(random.choices(population=string.ascii_letters, k=12))
-    temporary_file_path = f"./{random_string}.wav"
-    buffer = io.BytesIO(filestream)
-    with open(temporary_file_path, "wb") as outfile:
-        outfile.write(buffer.getvalue())
-    buffer.close()
+def predict(sound_data: bytes) -> dict[str, float]:
+    """Predict the genre corresponding to the passed sound data
 
-    image_buffer = io.BytesIO()
-    convert_sound_to_image(temporary_file_path).save(image_buffer, format="PNG")
-    os.remove(temporary_file_path)
-    image_bytes = image_buffer.getvalue()
-    decoded_image = tf.io.decode_image(image_bytes)
-    tensor = tf.ragged.stack([decoded_image])
-    result = model.predict(tensor)[0]
-    result = {labels_to_str.get(i): value for i, value in enumerate(result)}
+    :param sound_data: bytes read from a supported audio file
+    :return: dict of genre-confidence pairs
+    """
+    model = get_model()
+    label_map = get_label_mappings()
+
+    input_tensor = construct_input_tensor(sound_data)
+    probabilities = model.predict(input_tensor)
+    flattened_probabilities = np.mean(probabilities, axis=0)
+
+    result = {
+        label_map.get(i, f"Genre {i}"): value for i, value in enumerate(flattened_probabilities)
+    }
     return result
 
 
+def construct_input_tensor(sound_data: bytes) -> tf.RaggedTensor:
+    """Construct an input tensor from the passed sound file data
+
+    :param sound_data: bytes read from a supported audio file
+    :return: a stacked ragged tensor
+    """
+    images = []
+    with NamedTemporaryFile() as sound_file:
+        sound_file.write(sound_data)
+        for image in generate_sound_images(
+            sound_file.name,
+            desired_segments_seconds=30,
+            duration=90,
+        ):
+            with io.BytesIO() as image_buffer:
+                image.save(image_buffer, format="PNG")
+                image_bytes = image_buffer.getvalue()
+                decoded_image = tf.io.decode_image(image_bytes)
+                images.append(decoded_image)
+    tensor = tf.ragged.stack(images)
+    return tensor
+
+
 @cache
-def load_model():
+def get_model():
     """Loads model, assumes it's at ./model"""
     model = tf.keras.models.load_model(
         filepath=Path(__file__).resolve().parent / "model",
         compile=True,
     )
     return model
+
+
+@cache
+def get_label_mappings():
+    """Loads numeric-to-string label decoder, assumes it's at ./model"""
+    labels_json = Path(__file__).resolve().parent / "model" / "labels.json"
+    with open(labels_json, "r") as labels:
+        parsed_json = json.load(labels)
+        label_mapper = {int(index): label for index, label in parsed_json.items()}
+        return label_mapper
