7feff1ec37a35184c9fdeb05d84575f7ed576544
Donato Quartuccia <66536458+donatosaur@users.noreply.github.com>
2022-11-19 13:49:50 -0500

    Patch dependencies globally
    Co-authored-by: Fahad Awan <66268023+fahaddd-git@users.noreply.github.com>

--- services/prediction-api/tests/conftest.py
+++ services/prediction-api/tests/conftest.py
@@ -1,15 +1,12 @@
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch
 
 import numpy as np
 import pytest
 from fastapi.testclient import TestClient
-from prediction_api.app import app
-
-DEPENDENCY_MODULE_PATH = "prediction_api.dependencies"
 
 
-@pytest.fixture()
-def patch_dependencies(monkeypatch):
+@pytest.fixture(scope="session", autouse=True)
+def patch_dependencies():
     random_probabilities = [
         [0.0927, 0.2683, 0.1282, 0.0623, 0.0345, 0.0043, 0.2325, 0.0697, 0.0009, 0.1065],
         [0.4735, 0.0282, 0.0713, 0.1054, 0.0514, 0.0306, 0.0952, 0.0064, 0.0370, 0.1009],
@@ -28,12 +25,22 @@ def patch_dependencies(monkeypatch):
         8: "classical",
         9: "hiphop",
     }
-    monkeypatch.setattr("prediction_api.dependencies", "get_model", mock_tensorflow_model)
-    monkeypatch.setattr("prediction_api.dependencies", "get_label_map", lambda: labels_to_str)
+    mock_label_map = MagicMock(return_value=labels_to_str)
+
+    # Path method inspired by the following stackoverflow answer:
+    # https://stackoverflow.com/questions/38765917/how-to-patch-globally-in-pytest/65928873#65928873
+    # Date: 11/19/22
+    with (
+        patch("prediction_api.dependencies.get_model", mock_tensorflow_model),
+        patch("prediction_api.dependencies.get_label_map", mock_label_map),
+    ):
+        yield
 
 
 @pytest.fixture(scope="session")
 def test_client() -> TestClient:
     """Mock FastAPI client, see: https://fastapi.tiangolo.com/tutorial/testing"""
+    from prediction_api.app import app
+
     with TestClient(app) as test_client:
         yield test_client
