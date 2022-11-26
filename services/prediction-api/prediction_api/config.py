b7a0c8587a66b94965e6664888aa5c9b6a8c91b2
Donato Quartuccia <66536458+donatosaur@users.noreply.github.com>
2022-11-26 13:09:17 -0500

    Dockerize Prediction API (#63)
    * Read env vars from shell
    * Dockerize Predition API

--- services/prediction-api/prediction_api/config.py
+++ services/prediction-api/prediction_api/config.py
@@ -1,19 +1,18 @@
 from functools import cache
 from pathlib import Path
 
-from pydantic import BaseConfig, BaseSettings, Field
+from pydantic import BaseSettings, Field
 
 
 class _Settings(BaseSettings):
     debug: bool = Field(False, description="Toggle file watcher to restart server on change")
-    host: str = "localhost"
+    host: str = "0.0.0.0"
     port: int = 8000
     static_content_dir: Path = Path(__file__).resolve().parent / "static"
     model_dir: Path = Path(__file__).resolve().parent / "model"
     labels_json: Path = Path(__file__).resolve().parent / "model/labels.json"
 
-    class Config(BaseConfig):
-        env_file = "../.env", ".env"
+    class Config:
         case_sensitive = False
 
 
